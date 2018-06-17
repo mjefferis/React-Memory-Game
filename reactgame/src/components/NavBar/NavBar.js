0910590aa531d8e5ea2d0ce50144f39621f67a85
Matt <mjosephjefferis@gmail.com>
2018-06-17 14:30:08 -0400

    initial file structure

---
+++ reactgame/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar(props) {
+	return(
+<ul className="nav nav-tabs nav-justified">
+<li>Click Game</li>
+<li>Click an Image to begin!</li>
+<li>Score</li>
+</ul>
+	)
+}
+
+
+
+export default NavBar;
